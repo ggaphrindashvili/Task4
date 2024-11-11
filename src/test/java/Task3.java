4318f575b153c835c8b1ea5239bea9814922a17a
Gocha Gaprindashvili <ggaprindashvili@credo.ge>
2024-11-11 22:44:51 +0400

    Task3

---
+++ src/test/java/Task3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("შეიყვანე რიცხვი: ");
+        int number = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+        scanner.close();
+    }
+}
