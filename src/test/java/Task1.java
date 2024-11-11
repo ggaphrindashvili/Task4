8924af60ab49160b5809e8e68ccaedc65ea80c07
Gocha Gaprindashvili <ggaprindashvili@credo.ge>
2024-11-11 22:12:36 +0400

    Task1

--- src/test/java/Task1.java
+++ src/test/java/Task1.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the score: ");
+        int Grade = scanner.nextInt();
+
+        if (Grade >= 90 && Grade <= 100) {
+            System.out.println("Grade A: " + Grade);
+
+
+
+        }else if (Grade >= 80 && Grade <= 89) {
+            System.out.println("Grade B: " + Grade);
+
+        }
+        else if (Grade >= 70 && Grade <= 79) {
+            System.out.println("Grade C: " + Grade);
+        }
+        else if (Grade >= 60 && Grade <= 69) {
+            System.out.println("Grade D: " + Grade);
+        }
+        else if (Grade >= 1 && Grade <= 60) {
+            System.out.println("Grade F: " + Grade);
+        }
+        scanner.close();
+    }
 }
